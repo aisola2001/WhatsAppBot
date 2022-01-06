b20c84df9bf0ac74dec14c8b2f2a71da3e2e89ed
aisola2001 <agustinezequielisola@gmail.com>
2022-01-06 10:26:57 -0300

    [ADD]Missing Files with change 'client' for 'bot' & 'message.from' for 'test_number'

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionData = require(SESSION_FILE_PATH);
 }
 
-const client = new Client({
+const bot = new Client({
     session: sessionData,
 });
 
-client.initialize();
+bot.initialize();
 
-client.on("qr", (qr) => {
+bot.on("qr", (qr) => {
     qrcode.generate(qr, {small: true});
 });
 
 // Guardar valores de sesión en el archivo luego de una autenticación exitosa
-client.on("authenticated", (session) => {
+bot.on("authenticated", (session) => {
     sessionData = session;
     fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err){
         if(err){
@@ -32,42 +32,42 @@ client.on("authenticated", (session) => {
 });
 
 // "Raisear" si la restauración de la sesión no tuvo éxito
-client.on("auth_failure", msg => {
+bot.on("auth_failure", msg => {
     console.error('¡FALLO EN LA AUTENTICACIÓN!', msg);
 })
 
-client.on("ready", () => {
-    console.log("¡Agustín ya está conectado!");
+bot.on("ready", () => {
+    console.log("¡El Bot ya está conectado y listo!");
 });
 
 let is_new_number = true;
-client.on("message", message => {
+bot.on("message", message => {
     let test_number = "549" + "" + "@c.us";
     if(message.from === test_number){
         if(is_new_number){
             is_new_number = false;
             welcome_message = "¡Hola!\n";
-            welcome_message += "\nSoy AIBot (por Azar & Isola) y soy el robot de CityGas 🤖\n";
+            welcome_message += "\nSoy AIBot (por Azar & Isola) y soy el robot de City Gas 🤖\n";
             welcome_message += "\nLe puedo ayudar con los siguientes temas:\n";
             welcome_message += "*A.* Consulta de Cliente por Número de Teléfono\n";
             welcome_message += "*B.* Envío de un Pedido\n";
             welcome_message += "*C.* Consulta de un Pedido\n";
             welcome_message += "\nPor favor, escriba la letra que corresponda a su elección. Gracias.";
-            client.sendMessage(message.from, welcome_message);
+            bot.sendMessage(test_number, welcome_message);
         }
         if(message.body.toUpperCase === "A"){
             consult_client_message = "Ha seleccionado _Consulta de Cliente por Número de Teléfono_";
-            client.sendMessage(message.from, consult_client_message);
+            bot.sendMessage(test_number, consult_client_message);
         }else if(message.body.toUpperCase === "B"){
             sending_an_order_message = "Ha seleccionado _Envío de un Pedido_";
-            client.sendMessage(message.from, consult_client_message);
+            bot.sendMessage(test_number, consult_client_message);
         }else if(message.body.toUpperCase === "C"){
             consult_order_message = "Ha seleccionado _Consulta de un Pedido_";
-            client.sendMessage(message.from, consult_client_message);
+            bot.sendMessage(test_number, consult_client_message);
         }else{
             bad_message = "¡Uy! No entendí 🤯\n";
             bad_message += "\nPor favor, vuelva a intentarlo.";
-            client.sendMessage(message.from, bad_message);
+            bot.sendMessage(test_number, bad_message);
         }
     }
 });
